9d510ca18f12fc607c4224264dd86ba8e1abb789
BesatHead <ileonmaslov@gmail.com>
2024-12-17 21:46:08 +1100

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,7 @@
+number_of_completed_homework = 12
+numberOfHoursSpent = 1.5
+course_name = 'python'
+time_for_one_assignment = (number_of_completed_homework * numberOfHoursSpent)
+print('Я выполнил', number_of_completed_homework, 'домашних заданий!', 'На одно задание трачу', numberOfHoursSpent, 'часа.', 'Все задания относятся к курсу', course_name)
+print(int(time_for_one_assignment)) 
+
