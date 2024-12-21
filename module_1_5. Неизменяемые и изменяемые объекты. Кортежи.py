bdda01c1e11ab54b3da4300ce11310b200fcb21b
BesatHead <ileonmaslov@gmail.com>
2024-12-21 11:35:24 +1100

    решение задания

---
+++ module_1_5. Неизменяемые и изменяемые объекты. Кортежи.py
@@ -0,0 +1,16 @@
+# Задание №2
+immutable_var = 1, 2, 3, 'Порядковый номер'
+immutable_var_2 =1,2,[1,2,(3*2)]
+print(immutable_var)
+print(immutable_var_2)
+# Задание №3
+try:
+    immutable_var[0] = 2
+except TypeError as a:
+    print('Ошибка при изменении кортежа:', a)
+print(immutable_var)
+# Задание №4
+mutable_list = 'Катана', 'Топор', ['Саи', 'Нунчаки']
+print(mutable_list)
+mutable_list[2][0] = "Катана"
+print(mutable_list)
