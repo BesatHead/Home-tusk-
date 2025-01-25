89c99d18c9865d97223edf560679f163a977e862
Besathead <ileonmaslov@gmail.com>
2025-01-26 10:42:28 +1100

    Решение домашнего задания "Атрибуты и методы объекта" + Решение "Специальные методы классов" + Решение"Перегрузка методов"

--- Атрибуты и методы объекта.py
+++ Атрибуты и методы объекта.py
@@ -20,6 +20,34 @@ class House:
 
         return f'Дом {self.name} имеет {self.number_of_floors} этажей'
 
+    def __eq__(self, other): # Перегрузка методов
+        return self.number_of_floors == other.number_of_floors
+
+    def __add__(self, other): # Перегрузка методов
+        return House(self.name, self.number_of_floors + other)
+
+    def __iadd__(self, other): # Перегрузка методов
+        self.number_of_floors += other
+        return self
+
+    def __radd__(self, other): # Перегрузка методов
+        return House(self.name, self.number_of_floors + other)
+
+    def __gt__(self, other): # Перегрузка методов
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other): # Перегрузка методов
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other): # Перегрузка методов
+        return self.number_of_floors != other.number_of_floors
+    def __lt__(self, other): # Перегрузка методов
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other): # Перегрузка методов
+        return self.number_of_floors <= other.number_of_floors
+
+print('___________Атрибуты и методы объекта_________________')  # Атрибуты и методы объекта
+
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h3 = House('ЖК Эльбрус', 10) # Специальные методы классов
@@ -28,10 +56,42 @@ h4 = House('ЖК Акация', 20) # Специальные методы кла
 h1.go_to(5)
 h2.go_to(10)
 
-print ('____________________________________________') # Специальные методы классов
+print ('___________Специальные методы классов_________________') # Специальные методы классов
 
 print(h3)
 print(h4)
 
 print(len(h3))
 print(len(h4))
+
+print ('________________Перегрузка методов__________________') # Перегрузка методов
+
+print(h3)
+
+print(h4)
+
+print(h3 == h4) # __eq__
+
+h3 = h3 + 10 # __add__
+
+print(h3)
+
+print(h3 == h4)
+
+h3 += 10 # __iadd__
+
+print(h3)
+
+h4 = 10 + h4 # __radd__
+
+print(h4)
+
+print(h3 > h4) # __gt__
+
+print(h3 >= h4) # __ge__
+
+print(h3 < h4) # __lt__
+
+print(h3 <= h4) # __le__
+
+print(h3 != h4) # __ne__
